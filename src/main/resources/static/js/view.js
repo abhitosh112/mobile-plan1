8bf5f6edc91f9322b7ae5ec7c6764ca187273cc2
PrathmeshBhujbal <112059360+PrathmeshBhujbal@users.noreply.github.com>
2022-09-22 13:35:56 +0530

    view by name addded

--- src/main/resources/static/js/view.js
+++ src/main/resources/static/js/view.js
@@ -3,6 +3,98 @@
 	
 	function getBYID()
      {
+			
+			console.log("outside");
+			var pname =document.getElementById('name').value;
+			
+			if(pname.length!=0)
+			{
+				var xhr = new XMLHttpRequest();
+				xhr.open("GET", "http://localhost:8080/mp", false);
+				xhr.send();
+				var mpList = xhr.response;
+				console.log(xhr.response);
+				
+				var mobilePlans = JSON.parse(mpList);
+				var flag = 0;
+				
+				console.log("outside");
+				
+			var tbody=document.querySelector('.table > tbody')
+		   
+		   
+					while(tbody.firstChild)
+		  {
+			 tbody.removeChild(tbody.firstChild);
+		  }
+		
+		    mobilePlans.forEach((currentElement,index,arr) =>{
+		     
+			console.log(currentElement);
+			if(currentElement.name==pname)
+			{
+				
+			var th=document.createElement('th');	
+			var tr=document.createElement('tr');
+			var td1=document.createElement('td');
+			td1.innerHTML=currentElement.id;
+			var td2=document.createElement('td');
+			td2.innerHTML=currentElement.name;
+			var td3=document.createElement('td');
+			td3.innerHTML=currentElement.description;
+			var td4=document.createElement('td');
+			td4.innerHTML=currentElement.validity;
+			var td5=document.createElement('td');
+			td5.innerHTML='<button type="button" class="btn btn-outline-danger" onclick=deletebyid('+currentElement.id+')><img class="image-icon" src="images/delete.png"></button></td>';
+			var td6=document.createElement('td');
+			td6.innerHTML="<form ><a href='http://localhost:8080/update.html?id="
+						+ currentElement.id
+						+ "&name="
+						+ currentElement.name
+						+ "&description="
+						+ currentElement.description
+						+ "&validity="
+						+ currentElement.validity
+						+ "'><button class='btn btn-outline-danger' id = " + currentElement.id + " type='button'><img class='image-icon' src='images/edit.png'></button></form>"
+			
+			
+			tr.appendChild(td1);
+			tr.appendChild(td2);
+			tr.appendChild(td3);
+			tr.appendChild(td4);
+			tr.appendChild(td5);
+			tr.appendChild(td6);
+		    tbody.appendChild(tr);
+		      flag=1;
+			}
+	});
+		 
+		if(flag == 1)
+		        {
+					alert("Data for ID  is fetched");
+				
+				}
+				else
+				{
+					alert("Mobile Plan for this ID is not present");
+				}
+					
+	}
+			
+			else
+			{
+				alert("Please Enter ID");
+			}
+			
+			
+			  			
+		 }    
+	
+	
+	
+	/*
+	function getBYID()
+     {
 			console.log("outside");
 			var userid =document.getElementById('id').value;
 			
@@ -13,12 +105,14 @@
 				xhr.send();
 
 				var mpList = xhr.response;
-				
+			   // var mobilePlans = JSON.parse(mpList);
 			
-				console.log(mpList);
-				if(mpList=="ID not present")
+				//console.log(mobilePlans.id);
+				//console.log(mpList.id);
+				
+				if(xhr.status==404)
 				{
-					alert("Mobile Plan for ID "+userid+" not present");
+					alert(xhr.response);
 				}
 				else
 				{
@@ -34,7 +128,14 @@
 				
 			function search()
 			{
-				var mobilePlans = JSON.parse(mpList);
+				//var mobilePlans = JSON.parse(mpList);
+					var userid =document.getElementById('id').value;
+				var xhr = new XMLHttpRequest();
+				xhr.open("GET", "http://localhost:8080/mp/"+userid, false);
+				xhr.send();
+
+				var mpList = xhr.response;
+					var mobilePlans = JSON.parse(mpList);
 				var flag = 0;
 				
 				console.log("outside");
@@ -79,80 +180,4 @@
 						flag=1;
 			}
 					
-			/*
-			if(userid.length!=0)
-			{
-				var xhr = new XMLHttpRequest();
-				xhr.open("GET", "http://localhost:8080/mp", false);
-				xhr.send();
-				var mpList = xhr.response;
-				var mobilePlans = JSON.parse(mpList);
-				
-				var flag = 0;
-				
-				console.log("outside");
-				
-				var tbody=document.querySelector('.table > tbody')
-		     
-		     
-				for (var i = 0; i < mobilePlans.length; i++)
-				{
-					if(mobilePlans[i].id==userid)
-					{
-						console.log(mobilePlans[i].id);
-						var tr=document.createElement('tr');
-						var td1=document.createElement('td');
-						td1.innerHTML=mobilePlans[i].id;
-						var td2=document.createElement('td');
-						td2.innerHTML=mobilePlans[i].name;
-						var td3=document.createElement('td');
-						td3.innerHTML=mobilePlans[i].description;
-						var td4=document.createElement('td');
-						td4.innerHTML=mobilePlans[i].validity;
-				
-				 		var td5=document.createElement('td');
-			            td5.innerHTML='<button type="button" class="btn btn-outline-danger" onclick=deletebyid('+mobilePlans[i].id+')><img class="image-icon" src="images/delete.png"></button></td>';
-			            var td6=document.createElement('td');
-			            td6.innerHTML="<form ><a href='http://localhost:8080/update.html?id="
-						+ mobilePlans[i].id
-						+ "&name="
-						+ mobilePlans[i].name
-						+ "&description="
-						+ mobilePlans[i].description
-						+ "&validity="
-						+ mobilePlans[i].validity
-						+ "'><button class='btn btn-outline-danger' id = " + mobilePlans[i].id + " type='button'><img class='image-icon' src='images/edit.png'></button></form>"
-				
-						tr.appendChild(td1);
-						tr.appendChild(td2);
-						tr.appendChild(td3);
-						tr.appendChild(td4);
-						tr.appendChild(td5);
-			            tr.appendChild(td6);
-						tbody.removeChild(tbody.childNodes[0]);
-						tbody.appendChild(tr);
-						//tbody.replaceChild(tr,tbody.childNodes[0]);
-						flag=1;
-						
-			  		}		
-	  			}
-	  			
-			
-		        if(flag == 1)
-		        {
-					alert("Data for ID "+userid+" is fetched");
-				}
-				else
-				{
-					alert("Mobile Plan for this ID is not present");
-				}
-					
-				}
-			else
-			{
-				alert("Please Enter ID");
-			}
 			*/
-			
-			
-		
